1959d5bee7fb5790ee1a81b35f946dff2bc77f23
Micael Souza <micael.ns@hotmail.com>
2026-03-31 10:20:52 -0300

    feat: Ajuste estrutura de pastas

---
+++ src/Application/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Domain.Entities;
+using Domain.ObjectValues;
+
+var type = new VehicleType(1, "Cavalinho Scanna");
+var plate = new Plate("ASD-1W56");
+
+var vehicle = new Vehicle(1, type, plate, 0, []);
+
+
+Console.WriteLine($"Hello, World! {vehicle.Plate} {vehicle.Mileage}", vehicle);
